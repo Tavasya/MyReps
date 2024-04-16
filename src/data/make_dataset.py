76df3d1cc552c5ac722a6d300b5209b0f4c05fbb
Tavasya Ganpati <tavasyag@gmail.com>
2024-04-16 10:03:09 -0700

    extracted data/visualizaiton

--- src/data/make_dataset.py
+++ src/data/make_dataset.py
@@ -149,9 +149,9 @@ data_merged.columns = [
     "gyr_x",
     "gyr_y",
     "gyr_z",
+    "participant",
     "label",
     "category",
-    "participant",
     "set",
 ]
 
@@ -169,9 +169,9 @@ sampling = {
     "gyr_x": "mean",
     "gyr_y": "mean",
     "gyr_z": "mean",
+    "participant": "last",
     "label": "last",
     "category": "last",
-    "participant": "last",
     "set": "last",
 }
 data_merged[:100].resample(rule = "200ms").apply(sampling)
